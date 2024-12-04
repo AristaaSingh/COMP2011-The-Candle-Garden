a19b708688141045fc4ba8bf5ad10b7f05c09800
Aristaa Singh <146640656+AristaaSingh@users.noreply.github.com>
2024-12-04 19:19:54 +0000

    made category pages

--- app/views.py
+++ app/views.py
@@ -6,8 +6,21 @@ from app.models import Candle, Category
 admin.add_view(ModelView(Candle, db.session))
 admin.add_view(ModelView(Category, db.session))
 
+@app.context_processor
+def inject_categories():
+    categories = Category.query.all()
+    return dict(categories=categories)
+
 @app.route('/')
 def home():
     candles = Candle.query.all()
     return render_template('home.html', candles=candles)
+
+@app.route('/category/<int:category_id>')
+def category_page(category_id):
+    # Fetch the category and its candles from the database
+    category = Category.query.get_or_404(category_id)
+    candles = category.candles  # Get all candles in this category
+    return render_template('category.html', category=category, candles=candles)
+
     
